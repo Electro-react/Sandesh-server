70add8c275f1b986457f11b5b63a2d7479788871
electro-react <yr9953@gmail.com>
2022-09-18 02:25:37 +0530

    adding port

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(server, {
 });
 
 app.use(cors());
-const PORT = 5001;
+const PORT = process.env.PORT ||5001;
 
 app.get('/', (req, res) => {
   res.send('Running');
