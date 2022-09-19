21e6c313b3724b744b90ba98a4df68294ab8dbbd
electro-react <yr9953@gmail.com>
2022-09-20 00:47:42 +0530

    redeploying on heroku

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = require('socket.io')(server, {
 });
 
 app.use(cors());
-const port = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Running');
